e6231de17570154e43a797df84bd124155ee2544
BenBabicka <ben.babicka@gmail.com>
2021-10-04 16:33:35 +1100

    BasicEnemyCombat

--- Assets/Scripts/Enemy/EnemyCombat.cs
+++ Assets/Scripts/Enemy/EnemyCombat.cs
@@ -4,15 +4,89 @@ using UnityEngine;
 
 public class EnemyCombat : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public enum CombatType { Melee, Ranged }
+    public CombatType combatType;
+    
+    public bool isAttacking;
 
-    // Update is called once per frame
+    private float attackRate;
+    private float attackRateTimer;
+    private float attackRange;
+    private bool canAttack;
+
+    private GameObject projectile;
+    private Transform firepoint;
+
+
+
     void Update()
     {
-        
+        RetrieveVariables();
+        PreAttack();
     }
+
+    void RetrieveVariables()
+    {
+        attackRate = GetComponent<EnemyController>().attackRate;
+        canAttack = GetComponent<EnemyController>().canAttack;
+        projectile = GetComponent<EnemyController>().projectile;
+        firepoint = GetComponent<EnemyController>().firepoint;
+        attackRange = GetComponent<EnemyController>().attackingDistance;
+    }
+
+    void PreAttack()
+    {
+        attackRateTimer += Time.deltaTime;
+
+        if (canAttack)
+        {
+            isAttacking = true;
+
+            if (attackRateTimer > attackRate)
+            {
+                if (combatType == CombatType.Melee)
+                {
+                    MeleeAttack();
+                }
+                else
+                {
+                    RangedAttack();
+                }
+                attackRateTimer = 0;
+            }
+        }
+        else
+        {
+            isAttacking = false;
+        }
+    }
+
+    void MeleeAttack()
+    {
+      //  Debug.Log("Melee Attack");
+
+        RaycastHit hit;
+        if(Physics.Raycast(firepoint.position, firepoint.forward, out hit, attackRange))
+        {
+            Debug.Log(hit.transform.name);
+        }
+
+        isAttacking = false;
+    }
+
+    void RangedAttack()
+    {
+        //  Debug.Log("Ramged Attack");
+
+        GameObject bullet = Instantiate(projectile, firepoint.position, transform.rotation);
+
+        isAttacking = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(firepoint)
+            Gizmos.DrawRay(firepoint.position, firepoint.forward);
+    }
+
 }
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -8,24 +8,33 @@ public class EnemyController : MonoBehaviour
 {
     #region Exposed Variables
     public float spottingDistance = 10f;
-    public float attackingDistance = 2f;
-    
+    [Space(5)]
+    [Header("Movement variables")]
     public float movementSpeed = 4f;
     public float turnSpeed = 120f;
     public float stoppingDistance = 1f;
 
+    [Space(5)]
+    [Header("Attacking variables")]
+    public float attackingDistance = 2f;
+    public float attackRate;
+    public GameObject projectile;
+    public Transform firepoint;
+    
+
+    [Space(5)]
+    [Header("Gizmo")]
     public Color enemyViewDistanceColour;
     public Color enemyAttackingDistanceColour;
-
     #endregion
 
     #region Private Variables
     [HideInInspector]
-    public bool attack;
+    public bool canAttack;
 
     private Transform target;
     private NavMeshAgent nav;
-
+    private EnemyCombat enemyCombat;
 
     #endregion
     void Start()
@@ -38,8 +47,9 @@ public class EnemyController : MonoBehaviour
         nav = GetComponent<NavMeshAgent>();
         //set the nav mesh variable to the controller variables
         nav.speed = movementSpeed;
-        nav.angularSpeed = turnSpeed;
+        nav.angularSpeed = 0;
         nav.stoppingDistance = stoppingDistance;
+        enemyCombat = GetComponent<EnemyCombat>();
     }
  
     void Update()
@@ -49,32 +59,40 @@ public class EnemyController : MonoBehaviour
 
     void SearchForTarget()
     {
-
-        //Check if the player is within the range of the spotting distance
-        if (Vector3.Distance(transform.position, target.position) <= spottingDistance)
+        if (!enemyCombat.isAttacking)
         {
-            MoveToTarget();
+            //Check if the player is within the range of the spotting distance
+            if (Vector3.Distance(transform.position, target.position) <= spottingDistance)
+            {
+                MoveToTarget();
+            }
+            else if (Vector3.Distance(transform.position, target.position) > spottingDistance)
+            {
+              //  Debug.Log("Search for target");
+                nav.isStopped = true;
+            }
         }
-        else if (Vector3.Distance(transform.position, target.position) > spottingDistance)
-        {
-            Debug.Log("Search for target");
+        else
             nav.isStopped = true;
-        }
     }
     void MoveToTarget()
     {
+        var lookPos = target.position - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turnSpeed);
 
         //Stop the enemy moving towards the player if with in attacking range
-        if(Vector3.Distance(transform.position, target.position) <= attackingDistance)
+        if (Vector3.Distance(transform.position, target.position) <= attackingDistance)
         {
-            Debug.Log("Attack player");
+           // Debug.Log("Attack player");
 
-            attack = true;
+            canAttack = true;
         }
         else if (Vector3.Distance(transform.position, target.position) > attackingDistance)//Contiune the enemy's path towards the player if not in attacking range
         {
-            Debug.Log("Move to target");
-            attack = false;
+          //  Debug.Log("Move to target");
+            canAttack = false;
             nav.isStopped = false;
             nav.SetDestination(target.position);
         }
