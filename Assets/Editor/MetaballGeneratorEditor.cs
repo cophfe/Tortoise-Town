390de838c9cbe443c5560d4a3f65e13a721c1db0
Connor <cphfet@gmail.com>
2021-11-05 15:06:38 +1100

    Added metaball mesh generator
    - metaball generator
    - metaball
    
    notes:
    - undo does not currently work in editor
    - resolution will exponentially increase the amount of triangles and vertices, do not increase it too much
    - if there are holes in your mesh increase the bounds- eventually this will auto generate but I need to find the calculation bounding box around a metaball's influence area (wherever it gives out a value of more than 0)

---
+++ Assets/Editor/MetaballGeneratorEditor.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MetaballGenerator))]
+public class MetaballGeneratorEditor : Editor
+{
+	MetaballGenerator generator;
+	SerializedProperty threshold;
+	SerializedProperty resolution;
+
+	private void Awake()
+	{
+		generator = (MetaballGenerator)target;
+		threshold = serializedObject.FindProperty("threshold");
+		resolution = serializedObject.FindProperty("resolution");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		EditorGUILayout.PropertyField(threshold);
+		EditorGUILayout.PropertyField(resolution);
+		EditorGUI.BeginChangeCheck();
+		var newExtents = EditorGUILayout.Vector3Field("Extents", generator.meshBounds.extents);
+		if (EditorGUI.EndChangeCheck())
+		{
+			generator.meshBounds.extents = newExtents;
+		}
+
+		if (GUILayout.Button("Add Mesh Sphere"))
+		{
+			GameObject newGameObject = new GameObject("MetaSphere");
+			newGameObject.transform.parent = generator.transform;
+			newGameObject.transform.localPosition = Vector3.zero;
+			newGameObject.AddComponent<Metaball>();
+		}
+
+		GUILayout.BeginHorizontal();
+			if (GUILayout.Button("Generate Mesh"))
+			{
+				if (generator.metaShapes.Count > 0)
+				{
+					generator.GenerateMeta();
+					generator.GenerateMesh();
+				}
+			}
+			if (GUILayout.Button("Clear Mesh"))
+			{
+				if (generator.metaShapes.Count > 0)
+				{
+					generator.GenerateMeta();
+					generator.GenerateMesh();
+				}
+			}
+		GUILayout.EndHorizontal();
+		serializedObject.ApplyModifiedProperties();
+	}
+}
